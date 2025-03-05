5083a2ef0effe55ae162850a5dc630cfff609eb9
DevDyna <78147916+DevDyna@users.noreply.github.com>
2025-03-05 20:22:31 +0100

    Initial commit

---
+++ src/main/java/com/devdyna/modname_id/Main.java
@@ -0,0 +1,15 @@
+package com.devdyna.modname_id;
+
+import net.neoforged.bus.api.IEventBus;
+import net.neoforged.fml.ModContainer;
+import net.neoforged.fml.common.Mod;
+
+@Mod(Main.MODID)
+public class Main {
+
+    public static final String MODID = "modname_id";
+
+    public Main(IEventBus modEventBus, ModContainer modContainer) {
+
+    }
+}
