e1efcb3e91f1dc82beda7d729bc4f6c6e5e9551e
DevDyna <andrearisorti2003@gmail.com>
2025-03-09 14:31:21 +0100

    config moved

--- src/main/java/com/devdyna/tiabplusplus/Config.java
+++ src/main/java/com/devdyna/tiabplusplus/Config.java
@@ -1,4 +1,4 @@
-package com.devdyna.tiabplusplus.core;
+package com.devdyna.tiabplusplus;
 
 import net.neoforged.neoforge.common.ModConfigSpec;
 
@@ -31,24 +31,44 @@ public class Config {
             .define("Alert",true);
 
             public static final ModConfigSpec.BooleanValue EVENT_RightClickBlock = builder
-            .comment("This event could generate time")
-            .define("status",true);
+            .comment("Right Click Block: This event could generate time")
+            .define("event_rcb",true);
+
+            public static final ModConfigSpec.BooleanValue EVENT_RightClickBlock_AIR = builder
+            .comment("Include empty hand as valid item")
+            .define("event_rcb_empty",false);
 
             public static final ModConfigSpec.BooleanValue EVENT_BreakEvent = builder
-            .comment("This event could generate time")
-            .define("status",true);
+            .comment("Block Broken: This event could generate time")
+            .define("event_bb",true);
+
+            public static final ModConfigSpec.BooleanValue EVENT_BreakEvent_AIR = builder
+            .comment("Include empty hand as valid item")
+            .define("event_bb_empty",false);
 
             public static final ModConfigSpec.BooleanValue EVENT_EntityInteract = builder
-            .comment("This event could generate time")
-            .define("status",true);
+            .comment("Entity Interact: This event could generate time")
+            .define("event_ei",true);
+
+            public static final ModConfigSpec.BooleanValue EVENT_EntityInteract_AIR = builder
+            .comment("Include empty hand as valid item")
+            .define("event_ei_empty",true);
 
             public static final ModConfigSpec.BooleanValue EVENT_LeftClickBlock = builder
-            .comment("This event could generate time")
-            .define("status",true);
+            .comment("Block Breaking: This event could generate time")
+            .define("event_lcb",false);
+
+            public static final ModConfigSpec.BooleanValue EVENT_LeftClickBlock_AIR = builder
+            .comment("Include empty hand as valid item")
+            .define("event_lcb_empty",false);
 
             public static final ModConfigSpec.BooleanValue EVENT_RightClickItem = builder
-            .comment("This event could generate time")
-            .define("status",true);
+            .comment("Right Click Item: This event could generate time")
+            .define("event_rci",true);
+
+            public static final ModConfigSpec.BooleanValue EVENT_RightClickItem_AIR = builder
+            .comment("Include empty hand as valid item")
+            .define("event_rci_empty",false);
 
             public static final ModConfigSpec.IntValue MIN_VALUE_TIME = builder
             .comment("Min value of ticks of rewarding")
@@ -57,6 +77,14 @@ public class Config {
             public static final ModConfigSpec.IntValue MAX_VALUE_TIME = builder
             .comment("Max value of ticks of rewarding")
             .defineInRange("max", 10000, 2, Integer.MAX_VALUE);
+
+            public static final ModConfigSpec.BooleanValue CHANCE_WARNING = builder
+            .comment("Show toast to inform that was configured an unbalanced chance rate")
+            .define("status",true);
+
+            public static final ModConfigSpec.BooleanValue SPAM_WARNING = builder
+            .comment("Show toast to inform actions failed due auto-clicker")
+            .define("status",true);
             
 
             public static final ModConfigSpec configBuilder = builder.build();
